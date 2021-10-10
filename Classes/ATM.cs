085a90cdd6ad8ea05b7e42b34e89b1b81526a9db
Daaargh <75450833+Daaargh@users.noreply.github.com>
2021-10-10 16:05:03 +0100

    Create ATM class

---
+++ Classes/ATM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApplication.Classes
+{
+    public class ATM
+    {
+        int balance;
+
+        public ATM(int atmBalance)
+        {
+            this.balance = atmBalance;
+        }
+    }
+}
