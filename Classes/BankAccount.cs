ca847dab9086e485e47b3fd83cee5199fb9c563c
Daaargh <75450833+Daaargh@users.noreply.github.com>
2021-10-10 13:52:01 +0100

    Initial commit

---
+++ Classes/BankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApplication.Classes
+{
+    public class BankAccount
+    {
+        string accountNumber;
+        string pin;
+        int balance;
+        int overdraftLimit;
+        int balanceLimit;
+
+        public BankAccount(string accountNumber, string pin, int balance, int overdraftLimit)
+        {
+            this.accountNumber = accountNumber;
+            this.pin = pin;
+            this.balance = balance;
+            this.overdraftLimit = overdraftLimit;
+            this.balanceLimit = overdraftLimit * (-1);
+        }
+
+        public int GetBalance()
+        {
+            Console.WriteLine(balance);
+            return this.balance;
+        }
+
+        public void WithDrawFunds(int amount)
+        {
+            if (balance < balanceLimit)
+            {
+                Console.WriteLine("FUNDS_ERR");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine(balance);
+            }
+        }
+
+        public bool CheckPIN(string pin)
+        {
+            return this.pin == pin;
+        }
+    }
+}
