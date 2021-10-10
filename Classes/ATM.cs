c1c83197158e6163b77723ffdc2184d130d63946
Daaargh <75450833+Daaargh@users.noreply.github.com>
2021-10-10 18:32:37 +0100

    Write ATM class
    Write ATM class and methods

--- Classes/ATM.cs
+++ Classes/ATM.cs
@@ -8,6 +8,35 @@ namespace ATMApplication.Classes
 {
     public class ATM
     {
+        int initialBalance;
+        BankAccount account;
 
+        public ATM(int initialBalance, BankAccount account)
+        {
+            this.initialBalance = initialBalance;
+            this.account = account;
+        }
+
+        public void WithdrawMoney(int amount)
+        {
+            if ((initialBalance - amount) < 0)
+            {
+                Console.WriteLine("FUNDS_ERR");
+                return;
+            }
+            else if (initialBalance == 0)
+            {
+                Console.WriteLine("ATM_ERR");
+            }
+            else
+            {
+                Console.WriteLine(account.WithDrawFunds(amount));
+            }
+        }
+
+        public void CheckAccountBalance()
+        {
+            Console.WriteLine(account.GetBalance());
+        }
     }
 }
